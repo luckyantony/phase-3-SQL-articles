2d90558c90251f4a1a14c6db753c830bbaec7717
luckyantony <luckyantonyofficial@gmail.com>
2025-05-27 09:36:55 +0300

    changes to the database

--- lib/db/schema.sql
+++ lib/db/schema.sql
@@ -1,19 +1,24 @@
-CREATE TABLE IF NOT EXISTS authors (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL
+DROP TABLE IF EXISTS articles;
+DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS magazines;
+
+CREATE TABLE authors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS magazines (
-    id INTEGER PRIMARY KEY,
+CREATE TABLE magazines (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     category TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS articles (
-    id INTEGER PRIMARY KEY,
+CREATE TABLE articles (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
-    author_id INTEGER,
-    magazine_id INTEGER,
+    content TEXT NOT NULL,
+    author_id INTEGER NOT NULL,
+    magazine_id INTEGER NOT NULL,
     FOREIGN KEY (author_id) REFERENCES authors(id),
     FOREIGN KEY (magazine_id) REFERENCES magazines(id)
 );
