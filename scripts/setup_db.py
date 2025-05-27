1334360f3efcac7901a2eb3cdeb5563d1184395d
luckyantony <luckyantonyofficial@gmail.com>
2025-05-27 09:42:27 +0300

    changes to the setup file

--- scripts/setup_db.py
+++ scripts/setup_db.py
@@ -1,12 +1,16 @@
-import sqlite3
-from lib.db.connection import get_connection
+# import sqlite3
+# from lib.db.connection import get_connection
 
-with open("lib/db/schema.sql") as f:
-    schema_sql = f.read()
+# with open("lib/db/schema.sql") as f:
+#     schema_sql = f.read()
 
-conn = get_connection()
-cursor = conn.cursor()
-cursor.executescript(schema_sql)
-conn.commit()
+# conn = get_connection()
+# cursor = conn.cursor()
+# cursor.executescript(schema_sql)
+# conn.commit()
+# print("Database setup complete.")
+# conn.close()
+
+import os
+os.system("sqlite3 articles.db < lib/db/schema.sql")
 print("Database setup complete.")
-conn.close()
